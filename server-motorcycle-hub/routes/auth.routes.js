e995682409386c6cf1a0568383b638d778af96d8
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-06-05 18:06:43 +0100

    deleted console.logs

--- server-motorcycle-hub/routes/auth.routes.js
+++ server-motorcycle-hub/routes/auth.routes.js
@@ -112,7 +112,6 @@ router.post("/login", (req, res, next) => {
 
         // Send the token as the response
         res.status(200).json({ authToken: authToken });
-        console.log(req.payload)
       } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
@@ -124,7 +123,6 @@ router.post("/login", (req, res, next) => {
 router.get("/verify", isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and is made available on `req.payload`
-  console.log(`req.payload`, req.payload);
 
   // Send back the token payload object containing the user data
   res.status(200).json(req.payload);
